65d17b468596e779abf7373936d740afc2acceef
mhdadil-kk <adilkk816@gmail.com>
2024-10-21 09:06:50 +0530

    google

--- passport.js
+++ passport.js
@@ -18,7 +18,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://adilkk.online/auth/google/callback",
+    callbackURL: "https://localhost:3000/auth/google/callback",
     passReqToCallback: true
 },
 async (request, accessToken, refreshToken, profile, done) => {
